eeb9ef8b406f9fae98e2d9fbd46c9c5309741d0b
Ani-Shakhkyan <Ani-Shakhkyan>
2022-01-24 14:24:49 +0400

    pushing

---
+++ Makefile
@@ -0,0 +1,29 @@
+NAME = pipex
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+HEADER = pipex.h
+SRC = pipex.c resources.c
+OBJ = $(SRC:c=o)
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@echo "\033[0;32m\n\nCompiling pipex..."
+	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ)
+	@echo "\n\033[0mDone !"
+
+%.o: %.c
+	@printf "\033[0;33mGenerating pipex objects... %-10.10s\r" $@
+	@${CC} ${CFLAGS} -c $< -o $@
+
+clean:
+	@echo "\033[0;31m\nDeleting objects..."
+	@rm -f $(OBJ)
+	@echo "\033[0m"
+
+fclean: clean
+	@echo "\nDeleting executable..."
+	@rm -f $(NAME)
+	@echo "\033[0m"
+
+re: fclean all
+
--- pipex.c
+++ pipex.c
@@ -1,23 +1,25 @@
 #include "pipex.h"
+#include <stdio.h>
 
-char *find_path(char *first, char **envp)
+char	*find_path(char *first, char **envp)
 {
-	int i;
-	char *path;
-	char *temp;
-	char *command;
+	int	i;
+	char	*path;
+	char	*temp;
+	char	*command;
 
 	i = 0;
-	while(envp[i])
+	while (envp[i])
 	{
-		if(!ft_strncmp(envp[i], "PATH=", 5))
+		if (!ft_strncmp(envp[i], "PATH=", 5))
 		{
 			path = envp[i] + 5;
-			temp = ft_strndup(path,ft_strchr(path,':'));
-			command = ft_strjoin(temp,first);
-			if(!access(command,F_OK))
+			temp = ft_strndup(path, ft_strchr(path,':'));
+			command = ft_strjoin(temp, first);
+			if (!access(command, F_OK))
 			{
-				return command;
+				free(temp);
+				return (command);
 			}
 			free(temp);
 			free(command);
@@ -25,69 +27,69 @@ char *find_path(char *first, char **envp)
 		}
 		i++;
 	}
-	return first;
-
+	return (first);
 }
 
-void first_path(char *cmd, char **envp)
+void	execute(char *cmd, char **envp)
 {
-	char **commands;
-	char *path;
+	char	**commands;
+	char	*path;
 
-	commands = ft_split(cmd,' ');
-	if(ft_strchr(commands[0],'/')>=0)
+	commands = ft_split(cmd, ' ');
+	if (commands[0][0] == '/')
 		path = commands[0];
 	else
-		path = find_path(commands[0],envp);
-	execve(path,commands,envp);
-	write(2,"Command could not be found",27);
+		path = find_path(commands[0], envp);
+	execve(path, commands, envp);
+	//printf("path is:%s\n", path);
+	write(2, "Command could not be found\n", 27);
 	exit(127);
 }
 
-void first_cmd(int input_fd, char *cmd, char **envp)
+void	first_cmd(int input_fd, char *cmd, char **envp)
 {
-	int pfd[2];
-	pid_t pid;
+	int	pfd[2];
+	pid_t	pid;
 
 	pipe(pfd);
 	pid = fork();
-	if(pid == 0)
+	if (pid == 0)
 	{
 		close(pfd[0]);
-		dup2(pfd[1],1);
-		if(input_fd == 0)
+		dup2(pfd[1], 1);
+		if (input_fd == 0)
 			exit(1);
 		else
-			first_path(cmd, envp);
-		
+			execute(cmd, envp);
 	}
 	else
 	{
 		close(pfd[1]);
-		dup2(pfd[0],0);
-		waitpid(pid,NULL,0);
+		dup2(pfd[0], 0);
+		waitpid(pid, NULL, 0);
 	}
 }
 
-int main(int argc, char **argv,char** envp){
-
-
-
-	if(argc == 5)
+int main(int argc, char **argv, char** envp)
+{
+	if (argc == 5)
 	{
 		int input;
 		int output;
 
-		input = open(argv[1],O_RDONLY);
-		output = open(argv[4],O_CREAT | O_WRONLY | O_TRUNC,
+		input = open(argv[1], O_RDONLY);
+		output = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC,
 				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-		if(input == -1 || output == -1)
+		if (input == -1 || output == -1)
+		{
 			perror("There was a problem with opening the files");
-		dup2(input,STDIN_FILENO);
-		dup2(output,STDOUT_FILENO);
+			return (1);
+		}
+		dup2(input, STDIN_FILENO);
+		dup2(output, STDOUT_FILENO);
 		first_cmd(input, argv[2], envp);
-		first_path(argv[3],envp);
+		execute(argv[3], envp);
 	}
 	else
-		write(2, "Number of arguments wasn't sufficient",37);
+		write(2, "Number of arguments wasn't sufficient\n", 38);
 }
